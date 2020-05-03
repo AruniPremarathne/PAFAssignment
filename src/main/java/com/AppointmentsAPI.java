706b95316ad5fac2bcb4ce1c8e664573566a7ebf
AruniPremarathne <premarathnearuni@gmail.com>
2020-05-03 22:24:07 +0530

    Second Commit

--- src/main/java/com/AppointmentsAPI.java
+++ src/main/java/com/AppointmentsAPI.java
@@ -11,6 +11,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import com.Appointment;
 
+
 @WebServlet("/AppointmentsAPI")
 public class AppointmentsAPI extends HttpServlet {
     private static final long serialVersionUID = 1L;
